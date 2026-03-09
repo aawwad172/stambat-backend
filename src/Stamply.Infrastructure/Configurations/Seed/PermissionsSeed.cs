2f3232de662a3b5fc9badf9c6c86dbcbad257d55
Ahmad Awwad <aawwad172@gmail.com>
2026-03-09 23:52:23 +0300

    feat(permissions): Seed new project-specific permissions

--- src/Stamply.Infrastructure/Configurations/Seed/PermissionsSeed.cs
+++ src/Stamply.Infrastructure/Configurations/Seed/PermissionsSeed.cs
@@ -12,6 +12,39 @@ public class PermissionsSeed : IEntityTypeConfiguration<Permission>
     {
         // TODO: Add the desired Permissions.
         builder.HasData([
+            // Tenants
+            new Permission { Id = AuthSeedConstants.PermissionIdTenantsView, Name = PermissionConstants.TenantsView, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdTenantsAdd, Name = PermissionConstants.TenantsAdd, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdTenantsEdit, Name = PermissionConstants.TenantsEdit, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdTenantsDelete, Name = PermissionConstants.TenantsDelete, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+
+            // Users
+            new Permission { Id = AuthSeedConstants.PermissionIdUsersView, Name = PermissionConstants.UsersView, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdUsersAdd, Name = PermissionConstants.UsersAdd, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdUsersEdit, Name = PermissionConstants.UsersEdit, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdUsersDelete, Name = PermissionConstants.UsersDelete, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+
+            // Invitations
+            new Permission { Id = AuthSeedConstants.PermissionIdInvitationsView, Name = PermissionConstants.InvitationsView, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdInvitationsAdd, Name = PermissionConstants.InvitationsAdd, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdInvitationsEdit, Name = PermissionConstants.InvitationsEdit, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdInvitationsDelete, Name = PermissionConstants.InvitationsDelete, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+
+            // Cards
+            new Permission { Id = AuthSeedConstants.PermissionIdCardsView, Name = PermissionConstants.CardsView, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdCardsAdd, Name = PermissionConstants.CardsAdd, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdCardsEdit, Name = PermissionConstants.CardsEdit, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdCardsDelete, Name = PermissionConstants.CardsDelete, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+
+            // Rewards
+            new Permission { Id = AuthSeedConstants.PermissionIdRewardsView, Name = PermissionConstants.RewardsView, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdRewardsAdd, Name = PermissionConstants.RewardsAdd, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdRewardsEdit, Name = PermissionConstants.RewardsEdit, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdRewardsDelete, Name = PermissionConstants.RewardsDelete, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+
+            // Scan
+            new Permission { Id = AuthSeedConstants.PermissionIdScanStamping, Name = PermissionConstants.ScanStamping, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId },
+            new Permission { Id = AuthSeedConstants.PermissionIdScanRedeem, Name = PermissionConstants.ScanRedeem, CreatedAt = AuthSeedConstants.SeedDateUtc, CreatedBy = AuthSeedConstants.SystemUserId }
         ]);
     }
 }
